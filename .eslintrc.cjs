6d29554ef581b77e916eff015027284dbd27216d
Marco Ramirez <marcoramirez@Marcos-MacBook-Air.local>
2023-08-02 17:08:17 +0300

    Fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+import { off } from "process";
+
 /** @type {import("eslint").Linter.Config} */
 const config = {
   parser: "@typescript-eslint/parser",
@@ -11,8 +13,8 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/no-floating-promises": "off",
+
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     " @typescript-eslint/no-unsafe-assignment": "off",
